e07a6d1bd34e1da818582cd029e62d8278087df6
riskaamelia-wd <95574503+riskaamelia-wd@users.noreply.github.com>
2023-10-30 11:31:13 +0700

    first commit

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand bg-dark">
+        <ul class="nav">
+            <li>
+                <a class="nav-link" href="{{  url('beranda')  }}">Home</a>
+            </li>
+            <li>
+                <a class="nav-link" href="{{  url('data-siswa')  }}">Data Siswa</a>
+            </li>
+            <li>
+                <a class="nav-link" href="{{  url('info-kegiatan')  }}">Info Kegiatan</a>
+            </li>
+        </ul>
+    </nav>
+    <!-- <div class="jumbotron text-center">
+
+    </div> -->
+    <div class="container">
+        <div class="p-5 mb-4 bg-light rounded-3 text-center">
+            <h1>Portal Informasi Siswa</h1>
+            <p>Selamat datang di Portal Informasi Siswa Camp 404!</p>
+            <a href="#" class="btn btn-dark">Info Kegiatan</a>
+            <a href="#" class="btn btn-primary">Data Siswa</a>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</body>
+</html>
